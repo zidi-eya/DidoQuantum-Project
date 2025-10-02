431cea838a5242bf8819fd433fb554eb2ce4b878
@eya.zidi <eya.zidi@iterates.be>
2025-10-02 13:24:11 +0100

    clean code

--- FrontEnd-Quantum/src/modules/pages/contact.vue
+++ FrontEnd-Quantum/src/modules/pages/contact.vue
@@ -1,9 +1,4 @@
-
-
 <template>
-
-
-
   <div class="navbar">
     <div class="logo">
       <app-logo class="q-mb-xl" />
@@ -12,58 +7,57 @@
     <div class="nav-links">
       <a href="/offer">Our offers</a>
       <a href="/about">About</a>
-      <a   href="/contact">Contact</a>
-
+      <a href="/contact">Contact</a>
     </div>
-      <q-btn
+    <q-btn
       color="primary"
-      rounded  class="q-pa-md q-my-lg "
+      rounded
+      class="q-pa-md q-my-lg"
       label="Get Started Now"
       @click="router.push({ name: RouteNames.SIGN_IN })"
-      />
+    />
   </div>
 
   <div class="contact-us">
     <h2 class="contact-title">Contact Us</h2>
     <p class="contact-description">
-      You have question about Dido Quantum or want to connect with us?
-
-      Book a call with the team or contact us by email.
+      You have question about Dido Quantum or want to connect with us? Book a call with
+      the team or contact us by email.
     </p>
   </div>
-
-
 
   <div class="contact-info">
     <h2 class="contact-title">Proudly developed in Belgium</h2>
     <div class="contact-cards">
       <div class="contact-card">
-        <div class="contact-icon">📍</div> <!-- Use an actual icon or icon font here -->
-        <div class="contact-details">Belgium, Germany, <br>U.S. (Arizona LLC)</div>
+        <div class="contact-icon">📍</div>
+        <!-- Use an actual icon or icon font here -->
+        <div class="contact-details">Belgium, Germany, <br />U.S. (Arizona LLC)</div>
       </div>
       <div class="contact-card">
         <div class="contact-icon">✉️</div>
-        <div class="contact-details">Send us an email<br><a href="mailto:contact@dido.com"> info@didoquantumpulse.com
-
-        </a></div>
+        <div class="contact-details">
+          Send us an email<br /><a href="mailto:contact@dido.com">
+            info@didoquantumpulse.com
+          </a>
+        </div>
       </div>
       <div class="contact-card">
         <div class="contact-icon">📞</div>
-        <div class="contact-details">Give us a call<br>+32 493 531 183</div>
+        <div class="contact-details">Give us a call<br />+32 493 531 183</div>
       </div>
     </div>
   </div>
 
-  <br/><br/>
+  <br /><br />
 
   <q-card class="custom-card">
     <q-separator inset />
     <q-card-section class="column q-px-md preview-card">
       <div class="contentN">
         <div class="contentN-container">
-          <div  class="content-textN">
-            <q-form greedy class="q-gutter-md full-width" >
-
+          <div class="content-textN">
+            <q-form greedy class="q-gutter-md full-width">
               <q-input
                 class="full-width"
                 standout="bg-primary text-white"
@@ -72,10 +66,10 @@
                 lazy-rules="ondemand"
                 :rules="GeneralRules.fieldRequired('Please enter your full name')"
                 hide-bottom-space
-            >
-            <template v-slot:prepend>
-              <q-icon name="eva-person-outline" />
-            </template>
+              >
+                <template v-slot:prepend>
+                  <q-icon name="eva-person-outline" />
+                </template>
               </q-input>
 
               <q-input
@@ -110,54 +104,52 @@
               </q-input>
 
               <q-btn
-              class="q-pa-md q-my-lg full-width"
-              rounded
-              label="Send"
-              type="submit"
-              color="primary"
-              icon-right="eva-log-in-outline"
-               />
-
-            </q-form >
-
+                class="q-pa-md q-my-lg full-width"
+                rounded
+                label="Send"
+                type="submit"
+                color="primary"
+                icon-right="eva-log-in-outline"
+              />
+            </q-form>
           </div>
-          <div  class="contentN-image" style="margin-left:30px ;">
+          <div class="contentN-image" style="margin-left: 30px">
+            <h4>Questions or Comments?</h4>
+            <p>
+              We know that our clients have unique needs. Send us a message, and we will
+              get back to you soon.
+            </p>
 
-              <h4>Questions or Comments?</h4>
-              <p>We know that our clients have unique needs. Send us a message, and we will get back to you soon.</p>
-
-              <p> Dido Quantum Pulse <br/>
+            <p>
+              Dido Quantum Pulse <br />
               East Doubletree Ranch Road, Scottsdale, Arizona 85258, United States
-              </p>
+            </p>
 
-              <h6>Hours</h6>
-                  Mon : 09:00 – 17:00 <br/>
+            <h6>Hours</h6>
+            Mon : 09:00 – 17:00 <br />
 
-                  Tues : 09:00 – 17:00 <br/>
+            Tues : 09:00 – 17:00 <br />
 
-                  Wed : 09:00 – 17:00 <br/>
+            Wed : 09:00 – 17:00 <br />
 
-                  Thu : 09:00 – 17:00<br/>
+            Thu : 09:00 – 17:00<br />
 
-                  Fri : 09:00 – 17:00<br/>
+            Fri : 09:00 – 17:00<br />
 
-                  Sat : Closed <br/>
+            Sat : Closed <br />
 
-                  Sun : Closed <br/>
-
-
+            Sun : Closed <br />
           </div>
         </div>
       </div>
     </q-card-section>
   </q-card>
 
-<br/><br/>
+  <br /><br />
 
   <div class="footer">
     <div class="footer-credits">
       <app-logo class="q-mb-xl" />
-
     </div>
     <div class="footer-links">
       <a href="#" class="footer-link">Terms & Conditions</a>
@@ -166,33 +158,25 @@
       <a href="/offer" class="footer-link">Offers</a>
       <a href="/contact" class="footer-link">Contact</a>
     </div>
-
   </div>
 </template>
 <script setup lang="ts">
-import { AuthRules, GeneralRules } from '@/utils/validation/rules';
-import AppLogo from '@/components/AppLogo.vue';
-import { ref, onMounted } from 'vue';
-import { QBtn, QInput,QForm, QIcon } from 'quasar'
-import { useExceptionHandling } from '@/composables/exception-handling';
-import RouteNames from '@/modules/auth/router/RouteNames';
-  import { useRouter } from 'vue-router';
+import { AuthRules, GeneralRules } from "@/utils/validation/rules";
+import AppLogo from "@/components/AppLogo.vue";
+import { ref, onMounted } from "vue";
+import { QBtn, QInput, QForm, QIcon } from "quasar";
+import { useExceptionHandling } from "@/composables/exception-handling";
+import RouteNames from "@/modules/auth/router/RouteNames";
+import { useRouter } from "vue-router";
 
-  const router = useRouter();
-  const email = ref('');
-  const description= ref('');
-  const fullName=ref('');
-  const { safeExecute, errors } = useExceptionHandling();
-  const fileUpload = ref();
-
-
-
-
-
-
+const router = useRouter();
+const email = ref("");
+const description = ref("");
+const fullName = ref("");
+const { safeExecute, errors } = useExceptionHandling();
+const fileUpload = ref();
 </script>
 <style lang="scss">
-
 .navbar {
   display: flex;
   align-items: center;
@@ -214,8 +198,8 @@ import RouteNames from '@/modules/auth/router/RouteNames';
 .button {
   flex: 0 0 auto; /* Fixe l'espace pour le bouton */
 }
- </style>
- <style lang="sass" scoped>
+</style>
+<style lang="sass" scoped>
 
  body
   display: flex
@@ -309,5 +293,4 @@ import RouteNames from '@/modules/auth/router/RouteNames';
 .contact-details
     font-size: 16px // Standard font size
     color: #333 // Dark color for readability
-
- </style>
+</style>
